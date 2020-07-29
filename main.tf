628ec0b50318d962c1ebd55ee2664e705d83a4e1
chejuro1 <46506190+chejuro1@users.noreply.github.com>
2020-07-28 22:47:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   version = "2.33.0"
-  access_key = ${aws_access_key_id}
+#   access_key = ${aws_access_key_id}
    
-  secret_key = ${aws_secret_access_key}
+#   secret_key = ${aws_secret_access_key}
   region = var.aws_region
 }
 
